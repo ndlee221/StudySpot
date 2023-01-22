545597b6931ebe2046e1c302b4e8f0934661b0fc
David <dgmurgulet@gmail.com>
2023-01-22 02:03:24 -0800

    test

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,13 +2,16 @@ import { Button, Form, Col, Row, ListGroup, Navbar, Card } from 'react-bootstrap
 import '../App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react';
+import man from "../images/man-character-thinking-free-vector.png"
+import camera from "../images/camera.png"
+import book from "../images/book.png"
 
 function Home({ setHome }) {
 
     return (
         <div>
              <div className="d-flex">
-                <div className="d-flex ">
+                <div className="d-flex">
                     <h3 className='mt-3 mx-4 title fs-1'>
                         StudySpots
                     </h3>
@@ -18,39 +21,31 @@ function Home({ setHome }) {
                     </div>
                 </div>
             </div>
-
-
-            <div className="body d-flex">
+            <div className="body d-flex threeSection">
                 <p className='fs-3 paragraph'>
                     Enter a UBC Location to get started
                 </p>
-
-
                 <ul className="about">
                     <li className='about-items'>
+                    <img class="picture1" src={man}></img>
                         <p>               
                             Write reviews about your UBC spaces
                         </p>
                     </li>
-
                     <li className='about-items'>
-                    
+                    <img class="picture2" src={camera}></img>
                         <p>
                             Search to find the best rated new study spots
                         </p>
                     </li>
-
                     <li className='about-items'>
+                    <img class="picture3" src={book}></img>
                         <p>
                             Read other peoples experiences
                         </p>
                     </li>
                 </ul>
-
-
             </div>
-
-
         </div>
     );
 }
