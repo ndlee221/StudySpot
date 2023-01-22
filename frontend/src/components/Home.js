1d6af6898f5adc6ddd770cb42a48dd5391cd2ac9
Babak <hadadybabak@gmail.com>
2023-01-21 20:18:35 -0800

    Half way done.

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,14 +1,55 @@
-import { Button, Form, Col, Row, ListGroup, Navbar } from 'react-bootstrap';
+import { Button, Form, Col, Row, ListGroup, Navbar, Card } from 'react-bootstrap';
 import '../App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react';
 
-function Home({setHome}) {
+function Home({ setHome }) {
 
     return (
         <div>
-            <Navbar> Test</Navbar>
-            <Button className="my-5">Test</Button>
+             <div className="d-flex">
+                <div className="d-flex ">
+                    <h3 className='mt-3 mx-4 title fs-1'>
+                        StudySpots
+                    </h3>
+                </div>
+                <div className="navbar-container">
+                    <div className="bar mt-4">
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="body d-flex">
+                <p className='fs-3 paragraph'>
+                    Enter a UBC Location to get started
+                </p>
+
+                <ul className="about">
+                    <li className='about-items'>
+                        <p>
+                            Write reviews about your UBC spaces
+                        </p>
+                    </li>
+
+                    <li className='about-items'>
+                        <p>
+                            Search to find the best rated new study spots
+
+                        </p>
+                    </li>
+
+                    <li className='about-items'>
+                        <p>
+                            Read other peoples experiences
+                        </p>
+                    </li>
+                </ul>
+
+
+            </div>
+
+
         </div>
     );
 }
--- frontend/src/components/Spot.js
+++ frontend/src/components/Spot.js
@@ -1,9 +1,42 @@
+import { Card } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Spot({setHome}) {
+
+function Spot({ setHome }) {
+
+    const createCards =
+        <Card style={{ width: '18rem' }} className="card">
+            <Card.Text >test</Card.Text>
+        </Card>
+
+
 
     return (
         <div>
-            Test
+            <div className="d-flex">
+                <div className="d-flex ">
+                    <h3 className='mt-3 mx-4 title fs-1'>
+                        StudySpots
+                    </h3>
+                </div>
+                <div className="navbar-container">
+                    <div className="bar mt-4">
+                    </div>
+                </div>
+            </div>
+
+            <div className="name-container ">
+                <p className="mt-5 name">
+                    Building Name
+                </p>
+                <a className="location ">Location</a>
+                <p>
+                    Busy Rating:
+                </p>
+                <div className="card-container">
+                    {createCards}
+                </div>
+            </div>
         </div>
     );
 }
