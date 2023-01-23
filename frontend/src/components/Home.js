2dcd2bf8eb86af5247d5f60bafc4bcabb8f5bf53
Noel Illing <illing.noel@gmail.com>
2023-01-22 17:54:00 -0800

    added icons

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,7 +3,10 @@ import "./home.css"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react';
 import locationData from "./Data.json"
-import SearchBar from "./SearchBar";
+import SearchBar from "./SearchBar"
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+import CreateIcon from '@mui/icons-material/Create';
+import RoomIcon from '@mui/icons-material/Room';
 
 function Home({ setHome }) {
 
@@ -16,6 +19,22 @@ function Home({ setHome }) {
                 <div className={"searchBlock"}>
                     <SearchBar placeholder={"Location name"} data={locationData}/>
                 </div>
+            </div>
+            <div className={"footer"}>
+                <ul className={"list"}>
+                    <li className={"item"}>
+                        Read other peoples experiences
+                        <AutoStoriesIcon></AutoStoriesIcon>
+                    </li>
+                    <li className={"item"}>
+                        Search to find the best rated new study spots
+                        <RoomIcon></RoomIcon>
+                    </li>
+                    <li className={"item"}>
+                        Write reviews about your UBC spaces
+                        <CreateIcon></CreateIcon>
+                    </li>
+                </ul>
             </div>
 
         </div>
